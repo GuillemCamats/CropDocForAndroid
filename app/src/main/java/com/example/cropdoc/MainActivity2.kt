73f959dd00594c64093a10bdede2d53db2556749
GuillemCamats <64152349+GuillemCamats@users.noreply.github.com>
2022-06-12 23:42:48 +0200

    Ai recognition done

--- app/src/main/java/com/example/cropdoc/MainActivity2.kt
+++ app/src/main/java/com/example/cropdoc/MainActivity2.kt
@@ -1,34 +1,42 @@
 package com.example.cropdoc
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
 import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
-import android.provider.MediaStore
-import android.view.View.INVISIBLE
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
 import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
-import java.io.File
-import kotlin.system.exitProcess
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cropdoc.ml.LiteModelDiseaseClassification1
+import com.example.cropdoc.ml.Modelpocho
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
 
 
 class MainActivity2 : AppCompatActivity() {
     var pantalla: ImageView ?= null
-    var imageSelected: Boolean ?= false
     var confirm: Button ?=null
     var data: Uri ?= null
+    var text: TextView ?= null
+    private var bitmap: Bitmap ?= null
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         title = "KotlinApp"
-        pantalla = findViewById<ImageView>(R.id.imageView)
+        pantalla = findViewById(R.id.imageView)
         val buttonClick = findViewById<Button>(R.id.buttonLoadPicture)
-        confirm = findViewById<Button>(R.id.buttonConfirm)
+        confirm = findViewById(R.id.buttonConfirm)
 
         buttonClick.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
@@ -37,18 +45,39 @@ class MainActivity2 : AppCompatActivity() {
         }
 
         confirm?.setOnClickListener {
-            val intent = Intent(this, MainActivity3::class.java)
-            intent.putExtra("imageUri",data.toString())
-            startActivity(intent)
-        }
+            val model = LiteModelDiseaseClassification1.newInstance(this)
+            text = findViewById(R.id.textView)
 
+            bitmap = bitmap?.copy(Bitmap.Config.ARGB_8888, true)
+            //val resized: Bitmap? = bitmap?.let { it1 -> Bitmap.createScaledBitmap(it1, 1, 10, false) }
+
+
+            val tfImage = TensorImage.fromBitmap(bitmap)
+
+            //val byteBuffer = tfImage.buffer
+            //val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1,10), DataType.FLOAT32)
+
+            //Log.d("shape", byteBuffer.toString())
+            //Log.d("shape", inputFeature0.buffer.toString())
+            //inputFeature0.loadBuffer(byteBuffer)
+
+            val outputs = model.process(tfImage)
+            val probability = outputs.probabilityAsCategoryList.apply { sortByDescending { it.score } }
+
+            text?.text = probability.toString()
+
+
+        }
     }
+    @RequiresApi(Build.VERSION_CODES.P)
     private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
             // There are no request codes
-            confirm?.visibility = VISIBLE
+            confirm?.isEnabled = true
             data = result.data?.data
-            pantalla?.setImageURI(data)
+            val source: ImageDecoder.Source? = data?.let { ImageDecoder.createSource(this.contentResolver, it) }
+            bitmap = source?.let { ImageDecoder.decodeBitmap(it) }
+            pantalla?.setImageBitmap(bitmap)
         }
     }
 }
